f8c62cb0ad19b44555b8551a9501135e1bf140f0
Roger Gomà <rogergoma@bihotz-studio.com>
2024-03-05 11:11:24 +0100

    Escapa comilla simple

--- src/app/contacto/page.js
+++ src/app/contacto/page.js
@@ -5,33 +5,10 @@ import Image from "next/image";
 import styles from "./contacto.module.css";
 
 export default function Contacto() {
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    try {
-      const response = await fetch("/api/contact", {
-        method: "post",
-        body: formData,
-      });
-
-      if (!response.ok) {
-        console.log("falling over");
-        throw new Error(`response status: ${response.status}`);
-      }
-      const responseData = await response.json();
-      console.log(responseData["message"]);
-
-      alert("Message successfully sent");
-    } catch (err) {
-      console.error(err);
-      alert("Error, please try resubmitting the form");
-    }
-  }
-
   return (
     <div className={styles.container}>
       <div className={styles.main}>
-        <h1>LET'S WORK TOGETHER!</h1>
+        <h1>{`LET'S WORK TOGETHER!`}</h1>
       </div>
       <div>
         <div className={styles.coverImage}>
