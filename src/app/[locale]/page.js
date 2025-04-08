f99fb6b0fb8e79798d834fda325df71e22ffca72
Roger Gomà <rogergoma@bihotz-studio.com>
2025-04-08 13:18:14 +0200

    Some fixes

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -6,6 +6,7 @@ import { BorderAnimation } from "@/components/BorderAnimation/BorderAnimation";
 import { Footer } from "@/components/layout/footer/Footer";
 import { getTranslations } from "@/utils/getTranslations";
 import { CloudinaryImage } from "@/components/CloudinaryImage/CloudinaryImage";
+import Projects from "@/components/Projects/Projects";
 
 export async function generateMetadata({ params }) {
   const { locale } = await params;
@@ -21,73 +22,75 @@ export default async function Home({ params }) {
   const t = await getTranslations(locale, "Home");
 
   return (
-    <div className={styles.container}>
-      <div className={styles.secondary}>
-        <BorderAnimation
-          right={{ reverse: true }}
-          className={styles.coverImage}
-        >
-          <div className={styles.containerImage}>
-            <Link href="/" className={styles.logo}>
-              <Logo />
-            </Link>
+    <>
+      <div className={styles.container}>
+        <div className={styles.secondary}>
+          <BorderAnimation
+            right={{ reverse: true }}
+            className={styles.coverImage}
+          >
+            <div className={styles.containerImage}>
+              <Link href="/" className={styles.logo} aria-label="Bihotz studio">
+                <Logo />
+              </Link>
+              <CloudinaryImage
+                priority
+                className={styles.image}
+                sizes="40vw"
+                media={{
+                  altImage: t.altImage,
+                  id: "portada_wcs0vl",
+                  fill: true,
+                  style: { objectFit: "cover" },
+                }}
+              />
+            </div>
+          </BorderAnimation>
+        </div>
+        <div className={styles.main}>
+          <BorderAnimation
+            bottom={{ duration: "2s" }}
+            className={styles.borderInfo}
+          >
+            <div className={styles.info}>
+              <h1 className={styles.title}>{t.title}</h1>
+              <h2 className={styles.description}>{t.description}</h2>
+            </div>
+          </BorderAnimation>
+          <div className={styles.imageSmallScreen}>
             <CloudinaryImage
               priority
               className={styles.image}
-              sizes="40vw"
+              sizes="100vw"
               media={{
                 altImage: t.altImage,
                 id: "portada_wcs0vl",
                 fill: true,
-                style: { objectFit: "cover" },
+                style: { objectFit: "cover", objectPosition: "bottom" },
               }}
             />
           </div>
-        </BorderAnimation>
-      </div>
-      <div className={styles.main}>
-        <BorderAnimation
-          bottom={{ duration: "2s" }}
-          className={styles.borderInfo}
-        >
-          <div className={styles.info}>
-            <h1 className={styles.title}>{t.title}</h1>
-            <h2 className={styles.description}>{t.description}</h2>
-          </div>
-        </BorderAnimation>
-        <div className={styles.imageSmallScreen}>
-          <CloudinaryImage
-            priority
-            className={styles.image}
-            sizes="100vw"
-            media={{
-              altImage: t.altImage,
-              id: "portada_wcs0vl",
-              fill: true,
-              style: { objectFit: "cover", objectPosition: "bottom" },
-            }}
-          />
+          <BorderAnimation
+            top={{ duration: "2s", className: styles.hideBoder }}
+            className={styles.footerBorder}
+          >
+            <Footer className={styles.contact} locale={locale} />
+          </BorderAnimation>
+          <BorderAnimation left={{ duration: "1s" }}>
+            <div className={styles.secondaryImage}>
+              <CloudinaryImage
+                sizes="30vw"
+                media={{
+                  altImage: t.altSecondaryImage,
+                  id: "home_1_gb050t",
+                  fill: true,
+                  style: { objectFit: "cover" },
+                }}
+              />
+            </div>
+          </BorderAnimation>
         </div>
-        <BorderAnimation
-          top={{ duration: "2s", className: styles.hideBoder }}
-          className={styles.footerBorder}
-        >
-          <Footer className={styles.contact} locale={locale} />
-        </BorderAnimation>
-        <BorderAnimation left={{ duration: "1s" }}>
-          <div className={styles.secondaryImage}>
-            <CloudinaryImage
-              sizes="30vw"
-              media={{
-                altImage: t.altSecondaryImage,
-                id: "home_1_gb050t",
-                fill: true,
-                style: { objectFit: "cover" },
-              }}
-            />
-          </div>
-        </BorderAnimation>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/[locale]/proyectos/page.js
+++ src/app/[locale]/proyectos/page.js
@@ -1,14 +1,5 @@
-import Link from "next/link";
-import { BorderAnimation } from "../../../components/BorderAnimation/BorderAnimation";
-
-import styles from "./proyectos.module.css";
-
-import projects from "@/mocks/projects.json";
-import { CloudinaryImage } from "@/components/CloudinaryImage/CloudinaryImage";
-import { getCldImageUrl } from "next-cloudinary";
-import { Video } from "@/components/Video/Video";
-import { isBot } from "@/utils/isBot";
 import { getTranslations } from "@/utils/getTranslations";
+import Projects from "@/components/Projects/Projects";
 
 export async function generateMetadata({ params }) {
   const { locale } = await params;
@@ -28,63 +19,8 @@ export async function generateMetadata({ params }) {
   };
 }
 
-export default async function Proyecto({ params }) {
+export default async function Proyectos({ params }) {
   const { locale } = await params;
-  const isRobot = await isBot();
 
-  return (
-    <div className={styles.projects}>
-      {Object.entries(projects).map(([name, project], index) => (
-        <BorderAnimation
-          key={name}
-          className={styles.container}
-          bottom={{
-            className: styles.bottomBorder,
-            reverse: index % 2 !== 0,
-          }}
-          right={{
-            className: styles.rightBorder,
-            reverse: index % 3 === 0,
-          }}
-        >
-          <Link href={`/${locale}/proyectos/${name}`}>
-            <div className={styles.card}>
-              <div className={styles.imageContainer}>
-                {project.mediaCover.desktop[0].type === "video" ? (
-                  <Video
-                    key={project.url}
-                    url={project.mediaCover.desktop[0].url}
-                    style={{ width: "100%", height: "auto" }}
-                    className={styles.image}
-                    muted
-                    playsInline
-                    poster={getCldImageUrl({
-                      src: project.mediaCover.desktop[0].coverId,
-                      width: project.mediaCover.desktop[0].width,
-                      height: project.mediaCover.desktop[0].height,
-                    })}
-                    isBot={isRobot}
-                  />
-                ) : (
-                  <CloudinaryImage
-                    priority={index < 3}
-                    media={project.mediaCover.desktop[0]}
-                    className={styles.image}
-                    sizes="(max-width: 640px) 100vw, (max-width: 1920px) 50vw, 33vw"
-                  />
-                )}
-              </div>
-              <div className={styles.info}>
-                <div className={styles.infoTitle}>
-                  <h3 className={styles.client}>{project.client}</h3>
-                  <p className={styles.type}>{project.creativeFields}</p>
-                </div>
-                <p className={styles.description}>{project.description}</p>
-              </div>
-            </div>
-          </Link>
-        </BorderAnimation>
-      ))}
-    </div>
-  );
+  return <Projects locale={locale} />;
 }
